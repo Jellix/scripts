6b97d05687ff8d2a664a46dacc60b9221ee856c3
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2020-06-17 12:47:53 +0200

    Added help to make script more user-friendly.
      (I AM friendly to my users! Usually.)

--- sign-tarball.sh
+++ sign-tarball.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env bash
 
 if [ "$#" -ne 1 ]; then
-    echo "No or more than one tag given. Please provide exactly one."
-    echo "Here's list:"
-    git tag
-    exit 1
+   echo "No or more than one tag given. Please provide exactly one."
+   echo "Here's a list:"
+   (echo "TAG|DATE" && git tag --sort="creatordate" --format="%(refname:short)|%(authordate:iso8601)") | column -s\| -t -x
+   exit 1
+fi
+
+if [[ "$1" = "-h" || "$1" = "--help" ]]; then
+   echo "Creates a tarball for the specified tag and signs it.
+
+  Usage:
+    $(basename "$0") <tag-name>
+
+    Should be run from inside the root of a git repository.
+
+    <tag-name> is expected to be in the format v<some-version-number>. The
+    leading 'v' will be stripped from the <tag-name> for further operations,
+    this part is referred to as version number in the text below.
+
+  Naming:
+    The tarball will be named after the last part of your current directory
+    (which is preferrably the project's name), and the version number extracted
+    from the tag.
+    The contents inside the tarball will be the git archive with the version
+    number prepended as path.
+
+  Output:
+    The tarball, resulting signature file (<tarball>.asc) and SHA-512 checksum
+    file (<tarball>.sha512) will be put into the current directory.
+
+  And that's about all the help you get from me.
+"
+   exit 0
 fi
 
 TAG="$1"
 VERSION=$(echo -n "$1" | sed s/^v//)
 
-# Temporarily create the tarball and sign it.
-# Checkout the tag.
+# Create the tarball and sign it.
 echo "Creating tarball and signing it..."
 
 PROJECT=$(basename $(pwd))
